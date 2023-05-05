63218656bc0ce577307d71afa6af76df1b3116c6
perpyderp <g4m3rall3n@gmail.com>
2023-05-05 12:31:40 -0400

    Remove express directory from git index

--- express/routes/creatureRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const creatureController = require('../controllers/creatureController');
-
-// Get all creatures
-router.get('/creatures', creatureController.getCreatures);
-router.get('/creatures/:name', creatureController.getCreatureByName);
-
-module.exports = router;
